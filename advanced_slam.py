b736de14c24a4d75c3394f55539b5eb0688527e1
aineera <36616209+aineera@users.noreply.github.com>
2026-09-22 07:38:08 -0700

    Delete advanced_slam.py

--- advanced_slam.py
+++
@@ -1,8 +0,0 @@
-# Advanced - Pose Estimation + Optimization
-def estimate_pose():
-    # robot ki location batata hai
-    x, y, angle = 1.5, 2.0, 90
-    print(f"Robot ab is jagah hai: x={x}, y={y}")
-    return x, y
-
-estimate_pose()
