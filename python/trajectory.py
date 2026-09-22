9093b23f8c18d499df34e4c9bf08f0322c595945
aineera <36616209+aineera@users.noreply.github.com>
2026-09-22 08:26:26 -0700

    Create trajectory.py

---
+++ python/trajectory.py
@@ -0,0 +1,8 @@
+# Trajectory - Robot kahan kahan gaya
+def show_path():
+    path = [(0,0), (1,1), (2,2)] # x,y location
+    print("Robot ka rasta:", path)
+    return path
+
+if __name__ == "__main__":
+    show_path()
