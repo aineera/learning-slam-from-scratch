298945ae40dc83bc38f54dd3260d46e953ecf276
aineera <36616209+aineera@users.noreply.github.com>
2026-09-22 07:31:10 -0700

    Delete slam.py

--- slam.py
+++
@@ -1,8 +0,0 @@
-# Main SLAM - Sab yahan se start hota hai
-from tracking import track_features
-from map import create_map
-
-print("SLAM Started...")
-features = track_features()
-my_map = create_map(features)
-print("Map ban gaya!")
