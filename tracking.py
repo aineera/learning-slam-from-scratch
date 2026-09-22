90698eaac99cd76c7aa4a57991b23bdd422a89be
aineera <36616209+aineera@users.noreply.github.com>
2026-09-21 21:41:37 -0700

    Create tracking.py

---
+++ tracking.py
@@ -0,0 +1,5 @@
+# Tracking - Feature kahan gaya ye dekhta hai
+def track_features():
+    # yahan camera se points track hote hain
+    print("Features track ho rahe hain...")
+    return [10, 20, 30] # ye points hain
